87a1987ddb9f805fffa6660bced8463e83bba2ac
Luca Tronchin <ltronky@gmail.com>
2014-01-06 11:17:34 +0100

    bugFix

--- src/it/sblt/GD100OL.java
+++ src/it/sblt/GD100OL.java
@@ -48,47 +48,47 @@ public class GD100OL {
 		
 		for (int i = 0; i < variableList.length-1; i++) {
 //(x + 3);
-			IntVar xp3 = VariableFactory.bounded(StringUtils.randomName(), 0, 200, solver);
+			IntVar xp3 = VariableFactory.bounded(StringUtils.randomName(), -100, 200, solver);
 			solver.post(ICF.arithm(xp3, "-",variableList[i], "=", 3));
 
-			IntVar xp3Mod = VariableFactory.bounded(StringUtils.randomName(), 0, 200, solver);
+			IntVar xp3Mod = VariableFactory.bounded(StringUtils.randomName(), -100, 200, solver);
 			solver.post(ICF.mod(xp3, dimensionIV, xp3Mod));
 			
-			IntVar xMod = VariableFactory.bounded(StringUtils.randomName(), 0, 200, solver);
+			IntVar xMod = VariableFactory.bounded(StringUtils.randomName(), -100, 200, solver);
 			solver.post(ICF.mod(variableList[i], dimensionIV, xMod));
 			
-			IntVar xMinusXMod = VariableFactory.bounded(StringUtils.randomName(), 0, 200, solver);
+			IntVar xMinusXMod = VariableFactory.bounded(StringUtils.randomName(), -100, 200, solver);
 			IntVar negXMod = VariableFactory.minus(xMod); 
-			IntVar rSum = VariableFactory.bounded(StringUtils.randomName(), 0, 200, solver);
+			IntVar rSum = VariableFactory.bounded(StringUtils.randomName(), -100, 200, solver);
 			solver.post(ICF.sum(new IntVar[]{variableList[i],negXMod}, rSum));
 			solver.post(ICF.arithm(rSum, "=", xMinusXMod));
 			
 			IntVar negXp3Mod = VariableFactory.minus(xp3Mod);
-			IntVar tSum = VariableFactory.bounded(StringUtils.randomName(), 0, 200, solver);
+			IntVar tSum = VariableFactory.bounded(StringUtils.randomName(), -100, 200, solver);
 			solver.post(ICF.sum(new IntVar[]{xp3,negXp3Mod}, tSum));
 			
 			solver.post(LogicalConstraintFactory.ifThen(ICF.arithm(tSum ,"=", xMinusXMod), ICF.arithm(variableList[i+1], "-", variableList[i] ,"=", 3)));
 			
 //(x - 3);
-			IntVar xm3 = VariableFactory.bounded(StringUtils.randomName(), 0, 200, solver);
+			IntVar xm3 = VariableFactory.bounded(StringUtils.randomName(), -100, 200, solver);
 			solver.post(ICF.arithm(xp3, "-",variableList[i], "=", -3));
 
-			IntVar xm3Mod = VariableFactory.bounded(StringUtils.randomName(), 0, 200, solver);
+			IntVar xm3Mod = VariableFactory.bounded(StringUtils.randomName(), -100, 200, solver);
 			solver.post(ICF.mod(xm3, dimensionIV, xm3Mod));
 			
 			IntVar negXm3Mod = VariableFactory.minus(xm3Mod);
-			IntVar tSumXM3 = VariableFactory.bounded(StringUtils.randomName(), 0, 200, solver);
+			IntVar tSumXM3 = VariableFactory.bounded(StringUtils.randomName(), -100, 200, solver);
 			solver.post(ICF.sum(new IntVar[]{xm3,negXm3Mod}, tSumXM3));
 			
-			Constraint and1 = LogicalConstraintFactory.and(ICF.arithm(xm3, ">", 0), ICF.arithm(tSumXM3 ,"=", xMinusXMod));
+			Constraint and1 = LogicalConstraintFactory.and(ICF.arithm(xm3, ">", -100), ICF.arithm(tSumXM3 ,"=", xMinusXMod));
 			solver.post(LogicalConstraintFactory.ifThen(and1, ICF.arithm(variableList[i+1], "-", variableList[i] ,"=", -3)));
 			
 
 //(x + 3*dim)			
-			IntVar xp3xDim = VariableFactory.bounded(StringUtils.randomName(), 0, 200, solver);
+			IntVar xp3xDim = VariableFactory.bounded(StringUtils.randomName(), -100, 200, solver);
 			solver.post(ICF.arithm(xp3xDim, "-",variableList[i], "=", 3*dim));
 			
-			IntVar xp3xDimMod = VariableFactory.bounded(StringUtils.randomName(), 0, 200, solver);
+			IntVar xp3xDimMod = VariableFactory.bounded(StringUtils.randomName(), -100, 200, solver);
 			solver.post(ICF.mod(xp3xDim, dimensionIV, xp3xDimMod));
 			
 			Constraint and2 = LogicalConstraintFactory.and(ICF.arithm(xp3xDim, "<", dim*dim), ICF.arithm(xp3xDimMod ,"=", xMod));
@@ -96,10 +96,10 @@ public class GD100OL {
 			
 			
 //(x - 3*dim)
-			IntVar xm3xDim = VariableFactory.bounded(StringUtils.randomName(), 0, 200, solver);
+			IntVar xm3xDim = VariableFactory.bounded(StringUtils.randomName(), -100, 200, solver);
 			solver.post(ICF.arithm(xm3xDim, "-",variableList[i], "=", -3*dim));
 			
-			IntVar xm3xDimMod = VariableFactory.bounded(StringUtils.randomName(), 0, 200, solver);
+			IntVar xm3xDimMod = VariableFactory.bounded(StringUtils.randomName(), -100, 200, solver);
 			solver.post(ICF.mod(xm3xDim, dimensionIV, xm3xDimMod));
 			
 			Constraint and3 = LogicalConstraintFactory.and(ICF.arithm(xm3xDim, ">", 0), ICF.arithm(xm3xDimMod ,"=", xMod));
@@ -107,56 +107,56 @@ public class GD100OL {
 			
 			
 //(x + 2*dim-2) && (x + 2*dim+2)
-			IntVar xp2Dim = VariableFactory.bounded(StringUtils.randomName(), 0, 200, solver);
+			IntVar xp2Dim = VariableFactory.bounded(StringUtils.randomName(), -100, 200, solver);
 			solver.post(ICF.arithm(xp2Dim, "-",variableList[i], "=", 2*dim));
-			IntVar xp2DimMod = VariableFactory.bounded(StringUtils.randomName(), 0, 200, solver);
+			IntVar xp2DimMod = VariableFactory.bounded(StringUtils.randomName(), -100, 200, solver);
 			solver.post(ICF.mod(xp2Dim, dimensionIV, xp2DimMod));
 			
 			IntVar negXp2DimMod = VariableFactory.minus(xp2DimMod);
-			IntVar sum1 = VariableFactory.bounded(StringUtils.randomName(), 0, 200, solver);
+			IntVar sum1 = VariableFactory.bounded(StringUtils.randomName(), -100, 200, solver);
 			solver.post(ICF.sum(new IntVar[]{xp2Dim,negXp2DimMod}, sum1));
 			
 			for (int j = 0; j < c1.length; j++) {
-				IntVar xpC1 = VariableFactory.bounded(StringUtils.randomName(), 0, 200, solver);
+				IntVar xpC1 = VariableFactory.bounded(StringUtils.randomName(), -100, 200, solver);
 				solver.post(ICF.arithm(xpC1, "-",variableList[i], "=", c1[j]));
 				
-				IntVar xpC1Mod2 = VariableFactory.bounded(StringUtils.randomName(), 0, 200, solver);
+				IntVar xpC1Mod2 = VariableFactory.bounded(StringUtils.randomName(), -100, 200, solver);
 				solver.post(ICF.mod(xpC1, dimension2IV, xpC1Mod2));
 				
-				IntVar xpC1Mod = VariableFactory.bounded(StringUtils.randomName(), 0, 200, solver);
+				IntVar xpC1Mod = VariableFactory.bounded(StringUtils.randomName(), -100, 200, solver);
 				solver.post(ICF.mod(xpC1, dimensionIV, xpC1Mod));
 				
 				IntVar negXpC1Mod = VariableFactory.minus(xpC1Mod);
-				IntVar sum2 = VariableFactory.bounded(StringUtils.randomName(), 0, 200, solver);
+				IntVar sum2 = VariableFactory.bounded(StringUtils.randomName(), -100, 200, solver);
 				solver.post(ICF.sum(new IntVar[]{xpC1Mod2,negXpC1Mod}, sum2));
 				
 				solver.post(LogicalConstraintFactory.ifThen(ICF.arithm(sum2, "=", sum1), ICF.arithm(variableList[i+1], "-", variableList[i] ,"=", c1[j])));
 			}
 			
 //(x + 2*dim-2) && (x + 2*dim+2)
-			IntVar xm2Dim = VariableFactory.bounded(StringUtils.randomName(), 0, 200, solver);
+			IntVar xm2Dim = VariableFactory.bounded(StringUtils.randomName(), -100, 200, solver);
 			solver.post(ICF.arithm(xm2Dim, "-",variableList[i], "=", -2*dim));
 			
 			IntVar xm2DimAbs = VariableFactory.abs(xm2Dim);
-			IntVar xm2DimAbsMod = VariableFactory.bounded(StringUtils.randomName(), 0, 200, solver);
+			IntVar xm2DimAbsMod = VariableFactory.bounded(StringUtils.randomName(), -100, 200, solver);
 			solver.post(ICF.mod(xm2DimAbs, dimensionIV, xm2DimAbsMod));
 			
 			IntVar negXm2DimAbsMod = VariableFactory.minus(xm2DimAbsMod);
-			IntVar sum4 = VariableFactory.bounded(StringUtils.randomName(), 0, 200, solver);
+			IntVar sum4 = VariableFactory.bounded(StringUtils.randomName(), -100, 200, solver);
 			solver.post(ICF.sum(new IntVar[]{xm2Dim,negXm2DimAbsMod}, sum4));
 			
 			for (int j = 0; j < c1.length; j++) {
-				IntVar xmC1 = VariableFactory.bounded(StringUtils.randomName(), 0, 200, solver);
+				IntVar xmC1 = VariableFactory.bounded(StringUtils.randomName(), -100, 200, solver);
 				solver.post(ICF.arithm(xmC1, "-",variableList[i], "=", -c1[j]));
 				
-				IntVar xmC1Mod2 = VariableFactory.bounded(StringUtils.randomName(), 0, 200, solver);
+				IntVar xmC1Mod2 = VariableFactory.bounded(StringUtils.randomName(), -100, 200, solver);
 				solver.post(ICF.mod(xmC1, dimension2IV, xmC1Mod2));
 				
-				IntVar xmC1Mod = VariableFactory.bounded(StringUtils.randomName(), 0, 200, solver);
+				IntVar xmC1Mod = VariableFactory.bounded(StringUtils.randomName(), -100, 200, solver);
 				solver.post(ICF.mod(xmC1, dimensionIV, xmC1Mod));
 				
 				IntVar negXmC1Mod = VariableFactory.minus(xmC1Mod);
-				IntVar sum3 = VariableFactory.bounded(StringUtils.randomName(), 0, 200, solver);
+				IntVar sum3 = VariableFactory.bounded(StringUtils.randomName(), -100, 200, solver);
 				solver.post(ICF.sum(new IntVar[]{xmC1Mod2,negXmC1Mod}, sum3));
 				
 				Constraint and4 = LogicalConstraintFactory.and(ICF.arithm(xmC1, ">", 0), ICF.arithm(sum3, "=", sum4));
